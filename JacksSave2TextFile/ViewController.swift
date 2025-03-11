91554ac375c25ad86941a28528462648f8f50fea
JackAnlauf <prentrepreneur29@gmail.com>
2025-03-11 13:53:16 -0500

    Initial Commit

---
+++ JacksSave2TextFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JacksSave2TextFile
+//
+//  Created by student on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
